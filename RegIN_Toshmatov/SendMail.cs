aa70cb8df345b82449a4a173dedf9a787281505a
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-26 08:28:04 +0500

    Разработанный класс отправки сообщения на почту.  Тошматов

---
+++ RegIN_Toshmatov/SendMail.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Toshmatov.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "zewvquznuxphtyvg"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
